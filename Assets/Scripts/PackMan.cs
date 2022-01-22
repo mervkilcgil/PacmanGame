636f64eace9168d99da687e9a51278423708108a
mervekilicgil <merve.kilicgil@fugo.com.tr>
2022-01-22 12:52:07 +0300

    init

---
+++ Assets/Scripts/PackMan.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class PackMan : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void Start()
+    {
+        animator.SetFloat("DirX", 0);
+        animator.SetFloat("DirY", 0);
+    }
+}
